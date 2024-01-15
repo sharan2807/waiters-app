e1b75b951e863ff76482e6f88466ab5c76a53551
sharan2807 <sharancold98@gmail.com>
2024-01-15 16:37:28 +0530

    Fetching data & showing on console

--- src/components/AddData.js
+++ src/components/AddData.js
@@ -1,17 +1,38 @@
+import React, { useState } from 'react';
+
 const AddData = (props) => {
+    const [id,setId]=useState('');
+    const [dish,setDish]=useState('');
+    const [price,setPrice]=useState('');
+    const [table,setTable]=useState('');
+
+
     const addDataHandler=(event)=>{
         event.preventDefault();
+        console.log(id,dish,price,table)
+    }
+    const idChangeHandler=(event)=>{
+        setId(event.target.value)
+    }
+    const dishChangeHandler=(event)=>{
+        setDish(event.target.value)
+    }
+    const priceChangeHandler=(event)=>{
+        setPrice(event.target.value)
+    }
+    const tableChangeHandler=(event)=>{
+        setTable(event.target.value)
     }
   return (
     <form onSubmit={addDataHandler}>
-      <label>Unique Order ID:</label>
-      <input id="orderId" type="text"></input>
-      <label>Enter Dish:</label>
-      <input id="dish" type="text"></input>
-      <label>Enter Price:</label>
-      <input id="price" type="number"></input>
-      <label for="table">Choose a Table:</label>
-      <select id="table" >
+      <label htmlFor="id" >Unique Order ID:</label>
+      <input id="orderId" type="text" onChange={idChangeHandler}></input>
+      <label htmlFor="dish">Enter Dish:</label>
+      <input id="dish" type="text" onChange={dishChangeHandler}></input>
+      <label htmlFor="price">Enter Price:</label>
+      <input id="price" type="number" onChange={priceChangeHandler}></input>
+      <label htmlFor="table" >Choose a Table:</label>
+      <select id="table" onChange={tableChangeHandler} >
         <option value="table1">Table 1</option>
         <option value="table2">Table 2</option>
         <option value="table3">Table 3</option>
